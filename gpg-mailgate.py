e01e5053740c0f2cf33ebe9518735421c192c06a
perennate <admin@perennate.com>
2013-09-27 22:21:55 -0400

    Release under GNU Lesser General Public License.

--- gpg-mailgate.py
+++ gpg-mailgate.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+#
+#	gpg-mailgate
+#
+#	This file is part of the gpg-mailgate source code.
+#
+#	gpg-mailgate is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU Lesser General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	gpg-mailgate source code is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#	GNU Lesser General Public License for more details.
+#
+#	You should have received a copy of the GNU Lesser General Public License
+#	along with gpg-mailgate source code. If not, see <http://www.gnu.org/licenses/>.
+#
+
 from ConfigParser import RawConfigParser
 from email.mime.base import MIMEBase
 import email
